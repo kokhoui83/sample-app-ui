ee8243dddbe73be532a845fd5f273be185df86a6
brian lim <kokhoui.lim@duerrdental.com>
2022-10-11 14:05:13 +0800

    Add qoute component. Update App.vue - replace Welcome component with Quote component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Quotes from './components/Quotes.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Qoute logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Random qoutes?" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Quotes />
   </main>
 </template>
 
